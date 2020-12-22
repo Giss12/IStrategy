d3b2f6dd6e7ca882565e68b8a8c8028489cce00c
Giss12 <36027903+Giss12@users.noreply.github.com>
2020-12-22 12:14:03 +0300

    Add files via upload

---
+++ IStrategy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public interface IStrategy
+    {
+        void Perform(Transform transform);
+    }
+
+
+    public class MoveForward : IStrategy
+    {
+        public void Perform(Transform transform)
+        {
+            transform.Translate(0, 1 * Time.deltaTime, 0);
+        }
+    }
+
+    public class Rotate : IStrategy
+    {
+        public void Perform(Transform transform)
+        {
+            transform.Rotate(0, 0, 30 * Time.deltaTime);
+        }
+    }
+
+    public class Emmit : IStrategy
+    {
+        public void Perform(Transform transform)
+        {
+            transform.gameObject.GetComponent<ParticleSystem>().Emit(1);
+        }
+    }
+
---
+++ Performer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Performer : MonoBehaviour
+{
+    public IStrategy ContextStrategy;
+
+    public void Context(IStrategy _strategy)
+    {
+        ContextStrategy = _strategy;
+    }
+    public void SetStrategy(int i)
+    {
+        if(i == 1)
+        {
+            Context(new MoveForward());
+        }
+        if(i == 2)
+        {
+            Context(new Rotate());
+        }
+        if(i == 3)
+        {
+            Context(new Emmit());
+        }
+    }
+
+    private void Update()
+    {
+        ContextStrategy?.Perform(this.transform);
+    }
+}
